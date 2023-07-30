0f085daccb2c53039ab696ab8e20abcb0558825b
kristina-abobutsoae <abobutsoaekristina@gmail.com>
2023-07-30 20:15:06 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+setGalleryHtml("beforeend", galleryElCreateMarkup(galleryItems));
+
+function galleryElCreateMarkup(galleryItems){
+    return galleryItems
+    .map(({ preview,original,description}) => {
+       return `<li class = "gallery__item">
+              <a class ="gallery__link" href="${original}">
+              <img
+              class ="gallery__image"
+              src="${preview}"
+              alt="${description}"
+              />
+              </a>
+       </li>`;
+    })
+    .join("");
+}
+
+function setGalleryHtml(position, gallery) {
+  galleryList.insertAdjacentHTML(position, gallery);
+}
+
+const llightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: "bottom",
+  download: "Download",
+
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector(`iframe`);
+const player = new Player(iframe);
+
+
+const CurrentTimeKey = `video-player-timekey`;
+function getCurrentTimeKey(timeKey){
+    localStorage.setItem(CurrentTimeKey,timeKey.second);
+}
+player.on('timeupdate', throttle(getCurrentTimeKey, 1000));
+
+const CurrentTimeValue =  localStorage.setItem(CurrentTimeKey) || 0;
+player.setCurrentTime(CurrentTimeValue);
