5f5302d83e76b2c1fafc8562af3f57ca95c2b607
kristina-abobutsoae <abobutsoaekristina@gmail.com>
2023-08-06 16:06:32 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,38 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector(`iframe`);
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
 
-const CurrentTimeKey = `video-player-timekey`;
-function getCurrentTimeKey(timeKey){
-    localStorage.setItem(CurrentTimeKey,timeKey.second);
-}
-player.on('timeupdate', throttle(getCurrentTimeKey, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
-const CurrentTimeValue =  localStorage.setItem(CurrentTimeKey) || 0;
-player.setCurrentTime(CurrentTimeValue);
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player
+  .setColor('#00adef')
+  .then(function (color) {
+    console.log('the color that was set: #00adef');
+  })
+  .catch(function (error) {
+    console.log('an error occurred setting the color');
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const FORM_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let formData = JSON.parse(localStorage.getItem(FORM_KEY)) || {};
+const { email, message } = form.elements;
+
+popularePage();
+
+function onFormInput(e) {
+  // e.preventDefault();
+  formData = { email: email.value, message: message.value };
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+function popularePage() {
+  if (formData) {
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  // formData.email = email.value;
+  // formData.message = message.value;
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert('Будь-ласка, заповніть всі поля!');
+  }
+  localStorage.removeItem(FORM_KEY);
+  e.currentTarget.reset();
+  formData = {};
+}
